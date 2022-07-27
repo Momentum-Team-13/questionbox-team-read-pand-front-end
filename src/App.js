1c208498df812a628b1de66184ab31e718d24907
samrespass <samrespass@gmail.com>
2022-07-27 13:49:05 -0400

    global styles added to css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="wrap">
+    <h1>I'm an H1 Header </h1>&nbsp;&nbsp;&nbsp;
+    <h2>I'm an H2 Header</h2>&nbsp;&nbsp;&nbsp;
+    <h3>I'm an H3 Header</h3>
+    <div className="textBox">
+    <p>I'm a paragraph! and I'm inside a text box!</p>
+    </div>
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Login() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+  
+  export default Login;
+  
