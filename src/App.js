859e6b880e8f1a5b61b5a0082e2137b79a5f8337
samrespass <samrespass@gmail.com>
2022-07-27 14:02:18 -0400

    styling pt 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="wrap">
-    <h1>I'm an H1 Header </h1>&nbsp;&nbsp;&nbsp;
-    <h2>I'm an H2 Header</h2>&nbsp;&nbsp;&nbsp;
-    <h3>I'm an H3 Header</h3>
+    <h1>I'm an H1 Header </h1>
     <div className="textBox">
     <p>I'm a paragraph! and I'm inside a text box!</p>
     </div>
