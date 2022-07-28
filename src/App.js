b0b646e80255bdf2db964987d55c5bdb4da854ce
samrespass <samrespass@gmail.com>
2022-07-28 16:37:10 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
     <button className="askButton">Ask a Question</button>
        <div className="wrap">
-       <h1>Choose Your Category!</h1>
+       <h1>Test your category!</h1>
        <div className="genreList">
          {dummy.map(dummy => (
            <div className="gameGenre"><a href={dummy.link} className="genreLink"><h2>{dummy.name}</h2></a></div>
