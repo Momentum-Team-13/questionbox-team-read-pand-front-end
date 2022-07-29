ad3a616dde79b32750a8c3482ef3ea2727a635fb
Jose Reyes <jreyesri@cbu.edu>
2022-07-29 11:31:21 -0400

    The navbar and home page are working

--- src/home.js
+++ src/home.js
@@ -1,15 +1,17 @@
 import dummy from './dummy-data.json'
 
 export default function Home() {
-    <div className="App">
-        <button className="askButton">Ask a Question</button>
-        <div className="wrap">
-            <h1>Choose Your Category!</h1>
-            <div className="genreList">
-                {dummy.map(dummy => (
-                    <div className="gameGenre"><a href={dummy.link} className="genreLink"><h2>{dummy.name}</h2></a></div>
-                ))}
+    return (
+        <div className="App">
+            <button className="askButton">Ask a Question</button>
+            <div className="wrap">
+                <h1>Choose Your Category!</h1>
+                <div className="genreList">
+                    {dummy.map(dummy => (
+                        <div className="gameGenre"><a href={dummy.link} className="genreLink"><h2>{dummy.name}</h2></a></div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
